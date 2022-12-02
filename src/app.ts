a874319411925f1d836c7c5e86feffc88522e4ab
Valery Vysotsky <feraljerboa@gmail.com>
2022-12-02 18:07:58 +0300

    #5 Add /help endpoint

--- src/app.ts
+++ src/app.ts
@@ -33,6 +33,24 @@ app.post('/json-to-xml', (req, res) => {
     }
 });
 
+app.all('/help', (_, res) => {
+    res.status(200).end(`
+        Welcome to JSON-XML-Converter service!
+        use HTTP POST request to /json-to-xml endpoint to convert json to xml, json must be passed inside request body.
+        use HTTP POST request to /xml-to-json endpoint to convert xml to json, xml must be passed inside request body.
+
+        If you are using xml tag parameters, tag value will be corresponding to "$t" element in json.
+
+        Examples:
+        1)  input: "<foo attr=\"value\">bar</foo>"
+            output: "{"foo":{"attr":"value","$t":"bar"}}"
+
+        2)  input "{"foo":{"attr":"value","$t":"bar"}}"
+            output: "<foo attr="value">bar</foo>"
+    `.replace(/^ +/gm, '')
+    );
+})
+
 app.listen(port, () => {
     console.log(`listening on http://localhost:${port}`);
 });
